8479abe86c0ad697b531c063d47116a85177b478
乐特 <lete0419@aliyun.com>
2020-07-07 11:41:41 +0800

    up酱

--- js/upjiang.js
+++ js/upjiang.js
@@ -5,6 +5,6 @@
  */
 
 /*添加图片top*/
-var top_up = "<img id="upj" class="upj" src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
+var top_up = "<img id='upj' class='upj' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
 /*添加到返回顶部按钮下*/
 document.getElementById("go-up").innerHTML += top_up;
