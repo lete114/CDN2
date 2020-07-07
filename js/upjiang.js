edb65ec455f33c601b2740f10b2394fb32006215
乐特 <lete0419@aliyun.com>
2020-07-07 11:32:56 +0800

    up酱

---
+++ js/upjiang.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Lete on 2020/6/19.
+ * 更新：2020/7/7
+ * 更新内容：可自定义css修改up酱样式
+ */
+
+/*添加图片top*/
+var top_up = "<img id="upj" class="upj" src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
+/*添加到返回顶部按钮下*/
+document.getElementById("go-up").innerHTML += top_up;
